5da90bc4dc2f0a752d931587058490c9660e8b05
rvinay88 <rvinay88@gmail.com>
2014-06-09 15:13:40 +0530

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,95 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['scripts/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded',
+          sourcemap: true,
+          lineNumbers: true,
+          noCache: true
+        },
+        files: {
+          'css/style.css': 'scss/style.scss',
+        },
+      }
+    },
+    sassdown: {
+      styleguide: {
+        options: {
+          assets: ['css/style.css'],
+          highlight: 'monokai'
+        },
+        files: [{
+          expand: true,
+          cwd: 'scss',
+          src: ['**/*.scss'],
+          dest: 'public/styleguide'
+        }]
+      }
+    },
+    autoprefixer: {
+      css: {
+        options: {
+          browsers: ['last 2 version'],
+          cascade: true,
+        },
+        src: 'css/style.css',
+        dest: 'css/style.css'
+      },
+    },
+    csscomb: {
+        options: {
+            config: 'csscomb.json'
+        },
+        files: {
+          'css/style.css': 'css/style.css'
+        },
+    },
+    watch: {
+      styleguide: {
+        files: ['scss/**/*.scss'],
+        tasks: ['sass', 'autoprefixer', 'csscomb', 'sassdown', 'watch'],
+        options: {
+         livereload: 1337,
+        }
+      },
+      foundation: {
+        files: ['bower_components/foundation/scss/**/*.scss'],
+        tasks: ['sass', 'watch'],
+      }
+
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('sassdown');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-csscomb');
+
+  grunt.registerTask('styleguide', ['sass', 'autoprefixer', 'csscomb', 'sassdown', 'watch'])
+};
+
