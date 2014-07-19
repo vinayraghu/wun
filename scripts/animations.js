54097bcacc480e371891eb8a8209db421a2f345a
rvinay88 <rvinay88@gmail.com>
2014-07-19 12:40:18 +0530

    Side nav changes for links
    Animate toggle sidebar width

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,7 +1,11 @@
 'use strict';
 // Animate sidebar on load
 $(document).on("ready", function () {
-  $(".sidenav li")
+  $(".sidenav li a")
     .velocity("transition.slideLeftIn", { stagger: 25 }, 250)
-    .velocity({ opacity: 1 }, 25)
+    .velocity({ opacity: 1 }, 25);
+  $("#collapse-sidebar").on("click", function () {
+    $(".sidebar-primary").toggleClass("is-collapsed");
+    $(".content-main").toggleClass("is-expanded");
+  });
 });
