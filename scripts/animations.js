4b4111ec8ded93ecb38db256260f105f6ad18455
rvinay88 <rvinay88@gmail.com>
2014-07-24 17:01:38 +0530

    Enhance sidenav animation

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,7 +1,7 @@
 'use strict';
 // Animate sidebar on load
 $(document).on("ready", function () {
-  $(".sidenav li a")
+  $(".sidenav li, .sidenav li a")
     .velocity("transition.slideLeftIn", { stagger: 25 }, 250)
     .velocity({ opacity: 1 }, 25);
   $("#collapse-sidebar").on("click", function () {
